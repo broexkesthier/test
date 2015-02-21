65e19e6e46f328f1cd2fcd588f120c0dacf3265a
Lisa <liz.thier@gmail.com>
2015-02-21 14:45:25 +0100

    Kursplan eingefügt

--- kurse.php
+++ kurse.php
@@ -5,14 +5,53 @@
             <div class="row">
                 <div class="col-md-5">
                     <h2>Kurse</h2>
-                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam.</p>
+                    <p>Hier findest du unseren Kursplan und weitere Informationen zu den verschiedenen Kursen.</p>
                 </div>
             </div>
         </div>
     </section>
     <section>
         <div class="container">
-            
+            <table class="table table-hover">
+                <thead>
+                    <th>MO</th>
+                    <th>DI</th>
+                    <th>MI</th>
+                    <th>DO</th>
+                    <th>FR</th>
+                    <th>SA</th>
+                    <th>SO</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>18 - 19 Uhr<br>Lay Soul (Eileen)</td>
+                        <td>18 - 19 Uhr<br>Breakdance (Valéeri)</td>
+                        <td>&nbsp;</td>
+                        <td>18 - 19 Uhr<br>Afro (Davis)</td>
+                        <td>&nbsp;</td>
+                        <td>17:30 - 18:30 Uhr<br>Pole Dance Fortgeschrittene (Carina)</td>
+                    </tr>
+                    <tr>
+                        <td>19:30 - 20:30 Uhr<br>Pole Dance Fortgeschrittene (Carina)</td>
+                        <td>19:15 - 20:15 Uhr<br>Contemporary (Eileen)</td>
+                        <td>19:30 - 20:30 Uhr<br>Pole Dance Fortgeschrittene/Mittel (Carina)</td>
+                        <td>19:30 - 20:30 Uhr<br>Pole Dance gemischt (Carina)</td>
+                        <td>19:15 - 20:15 Uhr<br>Akrobatik (Davis)</td>
+                        <td>&nbsp;</td>
+                        <td>18:30 - 19:30 Uhr<br>Pole Dance Mittel (Carina)</td>
+                    </tr>
+                    <tr>
+                        <td>20:30 - 21:30 Uhr<br>Stretching (Carina)</td>
+                        <td>20:30 - 21:30 Uhr<br>Akrobatik (Benno)</td>
+                        <td>20:30 - 21:30 Uhr<br>Pole Dance Mittel/Anfänger (Carina)</td>
+                        <td>20:30 - 21:30 Uhr<br>Stretching (Carina)</td>
+                        <td>20:30 - 21:30 Uhr<br>Sexy Pole and Floor (Davis)</td>
+                        <td>&nbsp;</td>
+                        <td>19:30 - 20:30 Uhr<br>Pole Dance Anfänger (Carina)</td>
+                    </tr>
+                </tbod>
+            </table>
         </div>
     </section>
 
