c060985fda21050f2345bd0b55225653312e6465
Lisa <liz.thier@gmail.com>
2015-02-22 14:39:38 +0100

    Tabelle responsive

--- kurse.php
+++ kurse.php
@@ -12,7 +12,7 @@
     </section>
     <section>
         <div class="container content">
-            <table class="table table-hover">
+            <table class="table table-hover kurse">
                 <thead>
                     <th>MO</th>
                     <th>DI</th>
@@ -25,30 +25,30 @@
                 <tbody>
                     <tr>
                         <td>&nbsp;</td>
-                        <td>18 - 19 Uhr<br>Lady Soul (Eileen)</td>
-                        <td>18 - 19 Uhr<br>Breakdance (Valéeri)</td>
+                        <td data-label="Dienstag">18 - 19 Uhr<br>Lady Soul (Eileen)</td>
+                        <td data-label="Mittwoch">18 - 19 Uhr<br>Breakdance (Valéeri)</td>
                         <td>&nbsp;</td>
-                        <td>18 - 19 Uhr<br>Afro (Davis)</td>
+                        <td data-label="Freitag">18 - 19 Uhr<br>Afro (Davis)</td>
                         <td>&nbsp;</td>
-                        <td>17:30 - 18:30 Uhr<br>Pole Dance Fortgeschrittene (Carina)</td>
+                        <td data-label="Sonntag">17:30 - 18:30 Uhr<br>Pole Dance Fortgeschrittene (Carina)</td>
                     </tr>
                     <tr>
-                        <td>19:30 - 20:30 Uhr<br>Pole Dance Fortgeschrittene (Carina)</td>
-                        <td>19:15 - 20:15 Uhr<br>Contemporary (Eileen)</td>
-                        <td>19:30 - 20:30 Uhr<br>Pole Dance Fortgeschrittene/Mittel (Carina)</td>
-                        <td>19:30 - 20:30 Uhr<br>Pole Dance gemischt (Carina)</td>
-                        <td>19:15 - 20:15 Uhr<br>Akrobatik (Davis)</td>
+                        <td data-label="Montag">19:30 - 20:30 Uhr<br>Pole Dance Fortgeschrittene (Carina)</td>
+                        <td data-label="Dienstag">19:15 - 20:15 Uhr<br>Contemporary (Eileen)</td>
+                        <td data-label="Mittwoch">19:30 - 20:30 Uhr<br>Pole Dance Fortgeschrittene/Mittel (Carina)</td>
+                        <td data-label="Donnerstag">19:30 - 20:30 Uhr<br>Pole Dance gemischt (Carina)</td>
+                        <td data-label="Freitag">19:15 - 20:15 Uhr<br>Akrobatik (Davis)</td>
                         <td>&nbsp;</td>
-                        <td>18:30 - 19:30 Uhr<br>Pole Dance Mittel (Carina)</td>
+                        <td data-label="Sonntag">18:30 - 19:30 Uhr<br>Pole Dance Mittel (Carina)</td>
                     </tr>
                     <tr>
-                        <td>20:30 - 21:30 Uhr<br>Stretching (Carina)</td>
-                        <td>20:30 - 21:30 Uhr<br>Akrobatik (Benno)</td>
-                        <td>20:30 - 21:30 Uhr<br>Pole Dance Mittel/Anfänger (Carina)</td>
-                        <td>20:30 - 21:30 Uhr<br>Stretching (Carina)</td>
-                        <td>20:30 - 21:30 Uhr<br>Sexy Pole and Floor (Carina)</td>
+                        <td data-label="Montag">20:30 - 21:30 Uhr<br>Stretching (Carina)</td>
+                        <td data-label="Dienstag">20:30 - 21:30 Uhr<br>Akrobatik (Benno)</td>
+                        <td data-label="Mittwoch">20:30 - 21:30 Uhr<br>Pole Dance Mittel/Anfänger (Carina)</td>
+                        <td data-label="Donnerstag">20:30 - 21:30 Uhr<br>Stretching (Carina)</td>
+                        <td data-label="Freitag">20:30 - 21:30 Uhr<br>Sexy Pole and Floor (Carina)</td>
                         <td>&nbsp;</td>
-                        <td>19:30 - 20:30 Uhr<br>Pole Dance Anfänger (Carina)</td>
+                        <td data-label="Sonntag">19:30 - 20:30 Uhr<br>Pole Dance Anfänger (Carina)</td>
                     </tr>
                 </tbod>
             </table>
