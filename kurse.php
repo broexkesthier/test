2b0066cb0db541438dae9b537b37465814a6a735
Stephanie Bröxkes <stephanie.broexkes@gmail.com>
2015-07-12 22:26:55 +0200

    Texte in Kursen 3-Spaltig

--- kurse.php
+++ kurse.php
@@ -14,19 +14,17 @@
         <div class="container content">
         <h2>Kurse</h2>
         <div class="row">
-            <div class="col-sm-6">
+            <div class="col-sm-4">
                 <h3>Pole Dance, der etwas andere Tanz auch für Dich!</h3>
                 <p>Pole Dance (engl. für Stangentanz) vereint unterschiedliche tänzerische und akrobatische Elemente zu einem ganzheitlichen, effektiven Körpertraining. Durch die vielseitigen Drehungen und Pole Dance Figuren werden Muskeln in sämtlichen Körperregionen aufgebaut, deine Fitness und Flexibilität werden erhöht. Pole Dance zeichnet sich durch grenzenlose Eleganz, Weiblichkeit und akrobatische  Sexiness aus, durch die dein Selbst- und Körperbewusstsein kontinuierlich gesteigert wird. Und das Beste: Wirklich jeder, unabhängig von Alter, körperlicher Fitness und Flexibilität, kann Pole Dance erlernen!</p>
                 <p>Unterteilt wird das Pole Training in die verschiedenen Level Anfänger, Mittel, Fortgeschritten und Gemischt, sodass Du Dich immer im richtigen Anforderungsniveau befindest.</p>
             </div>
-            <div class="col-sm-6">
+            <div class="col-sm-4">
                 <h3>Sexy Pole and Floor</h3>
                 <p>Ergänzend zu unserem Pole Dance Angebot bieten wir Dir zusätzlich die beiden Kurse Sexy Pole & Floor und Stretch. In diesen Trainingseinheiten stehen die Stärkung des Selbstbewusstseins, das Erlernen oder Verbessern des eigenen Körpergefühls und die Ermittlung der persönlichen körperlichen Grenzen im Mittelpunkt.</p>
                 <p>Sexy Pole & Floor zeichnet sich durch einen sexy und sinnlichen Tanz aus, in dem die bis dahin erlernten Pole Dance Bewegungen/Figuren mit Boden- und Choreografie-Elementen kombiniert werden. Du lernst Deinen Körper kontrolliert und sinnlich zu unterschiedlicher Musik zu bewegen. Von zentraler Bedeutung ist in diesem Kurs das Herausstellen Deiner Weiblichkeit, insbesondere durch laszive Bewegungsabläufe und High Heel Training.</p>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-6">
+            <div class="col-sm-4">
                 <h3>Stretch</h3>
                 <p>In dem Zusatzkurs Stretch werden Elemente aus Yoga, Pilates, Ballett, Zirkus und dem traditionellen Kontorsionstraining in unterschiedlichen Übungen miteinander vereint. Durch diese werden deine Muskeln, Bänder und Sehnen sanft gedehnt, ohne die Gelenke zu belasten. Die Steigerung deiner ganzheitlichen Flexibilität steht dabei im Vordergrund, um eine saubere Ausführung der bisher erlernten Pole Dance Figuren zu vereinfachen und persönliche Ziele, wie bspw. Spagat, Brücke oder Ähnliches, zu erreichen.</p>
                 <p>Mit Hilfe diverser Hilfsgegenstände, wie Yogablöcke oder Yogagürtel, und ausgewählten Partnerübungen lernst Du in kontrollierten Bewegungsabläufen deine individuellen körperlichen Grenzen kennen und erweitern. Viele der erlernten Übungen kannst Du als Trainingsergänzung auch Zuhause durchführen.</p>
