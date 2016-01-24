fffa710b321a45e7de77c01112a68a119762eed5
Lisa <liz.thier@gmail.com>
2016-01-24 13:34:49 +0100

    Kontakt umbeenen

---
+++ kontakt_old.php
@@ -0,0 +1,27 @@
+<?php include 'head.php'; ?>
+
+    <section class="jumbotron">
+    	<div class="container">
+    		<div class="row">
+                <div class="col-md-6">
+			        <h2>Kontakt</h2>
+			        <p>Wir freuen uns auf Dich!</p>
+		    	</div>
+	    	</div>
+    	</div>
+    </section>
+    <section>
+        <div class="container content">
+            <h2>Anschrift</h2>
+            <div class="row">
+                <div class="col-md-8">
+                    <p>Tanzloft NRW<br>Sittarder Straße 63<br>41748 Viersen</p>
+                    <p>E-Mail: <a href="mailto:info@tanzloft-nrw.de">info@tanzloft-nrw.de</a></p>
+                </div>
+                <!--<div class="col-md-4">
+                    <p>- gute parkmöglichkeiten</p>
+                    <p>- gute verkehrsanbindung</p>
+                </div>-->
+            </div>
+    </section>
+<?php include 'footer.php'; ?>  
